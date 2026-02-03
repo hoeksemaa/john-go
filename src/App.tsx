7bfe2fd0c26438c9347d8ff32548e3f1dabd38b1
John Hoeksema <john@Johns-MacBook-Pro.local>
2026-02-03 16:00:09 -0500

    grid with values is displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createGame, makeMove } from "./go";
 
 function App() {
   let [gameState, setGameState] = useState(getInitialGame())
 
   // TODO: display the gameState, and call `makeMove` when a player clicks a button
-  return <div>Hello World! current player: {gameState.currentPlayer}</div>;
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>{gameState.board[0] ?? "_"}</td><td>{gameState.board[1] ?? "_"}</td><td>{gameState.board[2] ?? "_"}</td>
+          </tr>
+          <tr>
+            <td>{gameState.board[3] ?? "_"}</td><td>{gameState.board[4] ?? "_"}</td><td>{gameState.board[5] ?? "_"}</td>
+          </tr>
+          <tr>
+            <td>{gameState.board[6] ?? "_"}</td><td>{gameState.board[7] ?? "_"}</td><td>{gameState.board[8] ?? "_"}</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>Hello World! current player: {gameState.currentPlayer}</p>
+    </div>
+  )
 }
 
 function getInitialGame() {
