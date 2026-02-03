b643cc786aa419743d03fd6d20f2285b8e9a267d
John Hoeksema <john@Johns-MacBook-Pro.local>
2026-02-03 16:41:16 -0500

    working clicking

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from "react";
-import { createGame, makeMove } from "./go";
+import { createGame, makeMove, type GameState } from "./go";
 
 function App() {
   let [gameState, setGameState] = useState(getInitialGame())
+
+  function handleClick(position: number) {
+    const nextState = makeMove(gameState, position)
+    setGameState(nextState)
+  }
 
   // TODO: display the gameState, and call `makeMove` when a player clicks a button
   return (
@@ -10,17 +15,23 @@ function App() {
       <table>
         <tbody>
           <tr>
-            <td>{gameState.board[0] ?? "_"}</td><td>{gameState.board[1] ?? "_"}</td><td>{gameState.board[2] ?? "_"}</td>
+            <td onClick={() => handleClick(0)}>{gameState.board[0] ?? "_"}</td>
+            <td onClick={() => handleClick(1)}>{gameState.board[1] ?? "_"}</td>
+            <td onClick={() => handleClick(2)}>{gameState.board[2] ?? "_"}</td>
           </tr>
           <tr>
-            <td>{gameState.board[3] ?? "_"}</td><td>{gameState.board[4] ?? "_"}</td><td>{gameState.board[5] ?? "_"}</td>
+            <td onClick={() => handleClick(3)}>{gameState.board[3] ?? "_"}</td>
+            <td onClick={() => handleClick(4)}>{gameState.board[4] ?? "_"}</td>
+            <td onClick={() => handleClick(5)}>{gameState.board[5] ?? "_"}</td>
           </tr>
           <tr>
-            <td>{gameState.board[6] ?? "_"}</td><td>{gameState.board[7] ?? "_"}</td><td>{gameState.board[8] ?? "_"}</td>
+            <td onClick={() => handleClick(6)}>{gameState.board[6] ?? "_"}</td>
+            <td onClick={() => handleClick(7)}>{gameState.board[7] ?? "_"}</td>
+            <td onClick={() => handleClick(8)}>{gameState.board[8] ?? "_"}</td>
           </tr>
         </tbody>
       </table>
-      <p>Hello World! current player: {gameState.currentPlayer}</p>
+      <p>current player: {gameState.currentPlayer}</p>
     </div>
   )
 }
