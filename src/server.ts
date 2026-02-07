48ec677892d55499867ce34a63f18b2b880f7c44
john <johnmarchoeksema@gmail.com>
2026-02-06 19:59:29 -0500

    small fixes to typescript types build pass

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import ViteExpress from "vite-express"
 import { createGame, makeMove, type GameState } from "./go.ts"
 import { type Games } from './types.ts'
 
-const port = process.env.PORT || 3000
+const port = parseInt(process.env.PORT as string) || 3000
 
 export let games: Games = new Map()
 export const app = express()
